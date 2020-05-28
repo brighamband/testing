967b62c817455af901885c4e688430e7e177e4fd
Brigham Andersen <brighamband@gmail.com>
2020-05-28 16:40:08 -0600

    Test React app working

---
+++ MenteePortal/login/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+# class Mentor(models.Model):
+    
+#     first_name = models.TextField()
+#     last_name = models.TextField()
+#     email = models.TextField(unique=True)
+#     password = models.TextField()
+#     age = models.TextField()
+#     gender = models.TextField()
+#     time_zone = models.CharField(max_length=5)
+#     availability  = models.TextField()
+
+#     def __str__(self):
+#         return self.first_name + self.last_name
