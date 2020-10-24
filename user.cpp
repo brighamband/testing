df7041a4aa66dd12791b024ea7306486f8041b6f
Brigham Andersen <brighamband@gmail.com>
2020-10-24 17:20:56 -0600

    Added bubble sort

---
+++ user.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class User {
+    private:
+        std::string name;
+        int age;
+    public:
+        User(string n, int a) {
+            name = n;
+            age = a;
+        }
+        std::string toString() const {
+            return "Name: " + name + "\nAge: " + to_string(age) + "\n";
+        }
+};
+
+int main()
+{
+    User user("billy", 22);
+    
+    cout << user.toString();
+
+    return 0;
+}
