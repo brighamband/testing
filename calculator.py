e45d7f0e45ef61939b72d5f2d6abccd18a82acb5
Brigham Andersen <brighamband@gmail.com>
2021-05-20 14:28:23 -0600

    Added python calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("Hello, welcome to the basic calculator!\n")
+
+first_num = int(input("Please enter in the 1st number: "))
+
+second_num = int(input("Please enter in the 2nd number: "))
+
+answer = first_num + second_num
+
+print(str(first_num) + " + " + str(second_num) + " = " + str(answer))
---
+++ node-script.js
@@ -0,0 +1,8 @@
+let testNum = 2
+
+testNum = 4
+
+testNum = 6
+
+testNum = 8
+console.log(testNum)
